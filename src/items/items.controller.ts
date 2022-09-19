ebb7519eeb87d0cb09ef3df1c5b3415a6fdd8185
Krakaw <41575888+Krakaw@users.noreply.github.com>
2022-09-19 21:40:06 +0200

    Add items index with logging example

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Injectable,
+  Logger,
+  Param,
+  Render,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
 
-@Controller('items')
-export class ItemsController {}
+@Injectable()
+@Controller('/home/:homeSlug')
+export class ItemsController {
+  private readonly logger = new Logger(ItemsController.name);
+
+  constructor(private itemsServer: ItemsService) {}
+
+  @Get('items')
+  @Render('layouts/items/index.hbs')
+  async getItems(@Param('homeSlug') homeSlug: string) {
+    this.logger.log(homeSlug);
+    return {
+      items: [{ value: 1 }, { value: 2 }],
+    };
+  }
+}
